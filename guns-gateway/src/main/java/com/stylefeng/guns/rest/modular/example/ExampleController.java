495277a38fc8fe11902865fbaa2bcbce44da01e0
zhangjingcheng <zhangjingcheng000@gmail.com>
2019-03-19 17:52:08 +0800

    编写注册，登录接口

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/example/ExampleController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/example/ExampleController.java
@@ -1,5 +1,6 @@
 package com.stylefeng.guns.rest.modular.example;
 
+import com.stylefeng.guns.rest.common.CurrentUser;
 import com.stylefeng.guns.rest.common.SimpleObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,8 +18,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ExampleController {
 
     @RequestMapping("")
-    public ResponseEntity hello(@RequestBody SimpleObject simpleObject) {
-        System.out.println(simpleObject.getUser());
+    public ResponseEntity hello() {
+    //public ResponseEntity hello(@RequestBody SimpleObject simpleObject) {
+        //System.out.println(simpleObject.getUser());
+        System.out.println(CurrentUser.getCurrentUser());
+        // userId -> key -> redis[userInfo] -> 30分钟
+        // userId -> 条件，去数据库取
         return ResponseEntity.ok("请求成功!");
     }
 }
---
+++ guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
@@ -0,0 +1,114 @@
+package com.stylefeng.guns.rest.modular.user;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.stylefeng.guns.api.user.UserAPI;
+import com.stylefeng.guns.api.user.UserInfoModel;
+import com.stylefeng.guns.api.user.UserModel;
+import com.stylefeng.guns.core.util.MD5Util;
+import com.stylefeng.guns.rest.common.persistence.dao.UserMapper;
+import com.stylefeng.guns.rest.common.persistence.dao.UserTMapper;
+import com.stylefeng.guns.rest.common.persistence.model.UserT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Service(interfaceClass = UserAPI.class)
+public class UserServiceImpl implements UserAPI{
+
+    @Autowired
+    private UserTMapper userTMapper;
+
+
+    @Override
+    public boolean register(UserModel userModel) {
+        //获取注册信息
+
+        //将注册信息实体转化为数据实体[user_t]
+        UserT userT = new UserT();
+        userT.setUserName(userModel.getUsername());
+        //userT.setUserPwd(userModel.getPassword()); //注意
+        userT.setEmail(userModel.getEmail());
+        userT.setAddress(userModel.getAddress());
+        userT.setUserPhone(userModel.getPhone());
+        // 创建时间和修改时间 -> current_timestamp
+
+        // 数据加密 【MD5混淆加密 + 盐值 -> Shiro加密】
+        String md5Password = MD5Util.encrypt(userModel.getPassword());
+        userT.setUserPwd(md5Password);
+        //将数据实体存入数据库
+        Integer insert = userTMapper.insert(userT);
+        if(insert > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public int login(String name, String password) {
+        // 根据登陆账号获取数据库信息
+        UserT userT = new UserT();
+        userT.setUserName(name);
+
+        UserT result = userTMapper.selectOne(userT);
+        // 获取到的结果，然后与加密以后的密码做匹配
+        if(result != null && result.getUuid() > 0){
+            String md5Password = MD5Util.encrypt(password);
+            if(result.getUserPwd().equals(md5Password)){
+                return result.getUuid();
+            }
+        }
+        return 0;
+    }
+
+
+    @Override
+    public boolean checkUsername(String username) {
+
+        EntityWrapper<UserT> entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("user_name", username);
+        Integer result = userTMapper.selectCount(entityWrapper);
+        if(result != null && result > 0){
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+
+
+    private UserInfoModel do2UserInfo(UserT userT){
+        UserInfoModel userInfoModel = new UserInfoModel();
+
+        userInfoModel.setHeadAddress(userT.getHeadUrl());
+        userInfoModel.setPhone(userT.getUserPhone());
+        userInfoModel.setUpdateTime(userT.getUpdateTime().getTime());
+        userInfoModel.setEmail(userT.getEmail());
+        userInfoModel.setUsername(userT.getUserName());
+        userInfoModel.setNickname(userT.getNickName());
+        userInfoModel.setLifeState(""+userT.getLifeState());
+        userInfoModel.setBirthday(userT.getBirthday());
+        userInfoModel.setAddress(userT.getAddress());
+        userInfoModel.setSex(userT.getUserSex());
+        userInfoModel.setBeginTime(userT.getBeginTime().getTime());
+        userInfoModel.setBiography(userT.getBiography());
+
+        return userInfoModel;
+    }
+
+    @Override
+    public UserInfoModel getUserInfo(int uuid) {
+        // 根据主键查询用户信息 [UserT]
+        UserT userT = userTMapper.selectById(uuid);
+        // 将UserT转换UserInfoModel
+        UserInfoModel userInfoModel = do2UserInfo(userT);
+        // 返回UserInfoModel
+        return userInfoModel;
+    }
+
+    @Override
+    public UserInfoModel updateUserInfo(UserInfoModel userInfoModel) {
+        return null;
+    }
+}
